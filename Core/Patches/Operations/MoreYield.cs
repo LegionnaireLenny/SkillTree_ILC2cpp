3e4a6cf886ea78a88818e83f9f75412fb1236750
LegionnaireLenny <LegionnaireLenny@tutanota.com>
2026-02-15 18:26:36 -0500

    Forgot to save changes before commiting

--- Core/Patches/Operations/MoreYield.cs
+++ Core/Patches/Operations/MoreYield.cs
@@ -1,21 +1,13 @@
 ﻿using HarmonyLib;
 using Il2CppFishNet;
 using Il2CppScheduleOne.Growing;
+using Il2CppScheduleOne.ItemFramework;
+using Il2CppScheduleOne.ObjectScripts;
 using MelonLoader;
 using UnityEngine;
 
 namespace SkillTree.Core.Patches.Operations
 {
-    /// <summary>
-    /// CHANGE QUALITY SYSTEM BY POT TYPE -- BETTER POT = BETTER QUALITY
-    /// </summary>
-
-
-
-    /// <summary>
-    /// ADD YIELD FROM PLANTS
-    /// </summary>
-
     [HarmonyPatch(typeof(Plant), "GrowthDone")]
     public static class MoreYield
     {
@@ -37,5 +29,72 @@ namespace SkillTree.Core.Patches.Operations
                 MelonLogger.Msg($"[GrowthDone_SmartBasePatch] No additives detected. Skill applied. New Base: {finalBase}");
             }
         }
+
+        [HarmonyPatch(typeof(Cauldron), "RpcLogic___FinishCookOperation_2166136261")]
+        [HarmonyPostfix]
+        public static void Postfix(Cauldron __instance)
+        {
+            if (Core.SkillData == null || Core.SkillData.MoreCauldronOutput == 0)
+                return;
+
+            if (InstanceFinder.IsServer)
+            {
+                QualityItemInstance qualityItemInstance = __instance.CocaineBaseDefinition.GetDefaultInstance(10) as QualityItemInstance;
+                qualityItemInstance.SetQuality(__instance.InputQuality);
+                __instance.OutputSlot.InsertItem(qualityItemInstance);
+            }
+        }
+
+        //// TODO: Cauldron_Double_Output_Patch, incorrectly activates on chemistry station
+        //[HarmonyPatch(typeof(QualityItemDefinition), "GetDefaultInstance", typeof(int))]
+        //public static class Patch_Cauldron_Double_Output
+        //{
+        //    [HarmonyPrefix]
+        //    public static void Prefix(QualityItemDefinition __instance, ref int quantity)
+        //    {
+
+        //        if (Core.SkillData.MoreCauldronOutput == 0) 
+        //            return;
+
+        //        if (quantity != SkillModifiers.CauldronBaseOutput)
+        //            return;
+
+        //        MelonLogger.Msg($"Cauldron_Double_Output_Patch enter: MoreCauldronOutput {Core.SkillData.MoreCauldronOutput} quantity {quantity} base {SkillModifiers.CauldronBaseOutput}");
+        //        if (__instance.name.Contains("CocaineBase"))
+        //        {
+        //            quantity = SkillModifiers.GetCauldronStackSize(); 
+        //        }
+        //    }
+        //}
+
+        [HarmonyPatch(typeof(MixingStation), "GetMixQuantity")]
+        [HarmonyPostfix]
+        public static void Postfix(MixingStation __instance, ref int __result)
+        {
+
+            if (__instance.GetProduct() == null || __instance.GetMixer() == null || Core.SkillData == null || Core.SkillData.MoreMixAndDryingRackOutput == 0)
+                return;
+
+            __result = Mathf.Min(Mathf.Min(__instance.ProductSlot.Quantity, __instance.MixerSlot.Quantity) * SkillModifiers.GetMixDryOutputMultiplier(),
+                __instance.MaxMixQuantity * SkillModifiers.GetMixDryOutputMultiplier());
+        }
+
+        [HarmonyPatch(typeof(DryingRack), "InitializeGridItem")]
+        [HarmonyPostfix]
+        public static void Postfix(DryingRack __instance)
+        {
+            //MelonLogger.Msg($"[DryingRack] Updating rack capacity.");
+            ApplyCapacityUpdate(__instance);
+        }
+
+        public static void ApplyCapacityUpdate(DryingRack __instance)
+        {
+            if (Core.SkillData == null || Core.SkillData.MoreMixAndDryingRackOutput == 0)
+                return;
+
+            __instance.ItemCapacity *= SkillModifiers.MixDryOutputSizeMultiplier;
+            __instance.RefreshHangingVisuals();
+        }
     }
 }
+
