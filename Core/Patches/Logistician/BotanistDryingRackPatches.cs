0c0e89fec77f27a1f2c317919c502499025b4a33
LegionnaireLenny <LegionnaireLenny@tutanota.com>
2026-03-25 13:08:56 -0400

    Faster Farmers now affects drying racks too

---
+++ Core/Patches/Logistician/BotanistDryingRackPatches.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using Il2CppFishNet;
+using Il2CppScheduleOne.NPCs.Behaviour;
+using MelonLoader;
+using SkillTree.Core.Skills;
+using System.Collections;
+using UnityEngine;
+
+namespace SkillTree.Core.Patches.Logistician
+{
+    [HarmonyPatch]
+    public class BotanistDryingRackPatches
+    {
+        [HarmonyPatch(typeof(StartDryingRackBehaviour), "RpcLogic___BeginAction_2166136261")]
+        [HarmonyPrefix]
+        public static bool RpcLogic___BeginAction_2166136261(StartDryingRackBehaviour __instance)
+        {
+            if (__instance.WorkInProgress)
+            {
+                return false;
+            }
+            if (__instance.Rack == null)
+            {
+                return false;
+            }
+            __instance.WorkInProgress = true;
+            __instance.Npc.Movement.FacePoint(__instance.Rack.uiPoint.position, 0.5f);
+            __instance.workRoutine = (Coroutine)MelonCoroutines.Start(BeginAction(__instance));
+
+            return false;
+        }
+
+        private static IEnumerator BeginAction(StartDryingRackBehaviour instance)
+        {
+            yield return new WaitForEndOfFrame();
+            instance.Rack.InputSlot.ItemInstance.GetCopy(1);
+            int itemCount = 0;
+            while (instance.Rack != null && instance.Rack.InputSlot.Quantity > itemCount && instance.Rack.GetTotalDryingItems() + itemCount < instance.Rack.ItemCapacity)
+            {
+                instance.Npc.Avatar.Animation.SetTrigger("GrabItem");
+                yield return new WaitForSeconds(1f * SkillModifiers.GetBotanistActionDurationMultiplier());
+                int num = itemCount;
+                itemCount = num + 1;
+            }
+            if (InstanceFinder.IsServer)
+            {
+                instance.Rack.StartOperation();
+            }
+            instance.WorkInProgress = false;
+            instance.workRoutine = null;
+            yield break;
+        }
+    }
+}
--- Core/Patches/Logistician/BotanistGetDurationPatches.cs
+++ Core/Patches/Logistician/BotanistGetDurationPatches.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 namespace SkillTree.Core.Patches.Logistician
 {
     [HarmonyPatch]
-    public class BotanistBehaviorPatches
+    public class BotanistGetDurationPatches
     {
         static IEnumerable<MethodBase> TargetMethods()
         {
