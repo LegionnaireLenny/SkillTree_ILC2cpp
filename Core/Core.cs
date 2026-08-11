089d611e0e00303c556ca58757d6acf79c33315f
LegionnaireLenny <LegionnaireLenny@tutanota.com>
2026-08-11 17:17:12 -0400

    Version 3.3.2

--- Core/Core.cs
+++ Core/Core.cs
@@ -21,7 +21,7 @@ using System.IO;
 using UnityEngine;
 using static SkillTree.Core.Utilities.ConfigManager;
 
-[assembly: MelonInfo(typeof(Core), "SkillTree", "3.3.1", "CrazyReizor & VindicatedVendetta", null)]
+[assembly: MelonInfo(typeof(Core), "SkillTree", "3.3.2", "CrazyReizor & VindicatedVendetta", null)]
 [assembly: MelonGame("TVGS", "Schedule I")]
 
 namespace SkillTree.Core
